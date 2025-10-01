6ccde572b45e5363996330242598235ca92ca455
Igor Ryazanov <123439439+Greypfrut13@users.noreply.github.com>
2025-10-01 13:38:02 +0800

    Created movement for Bullet

--- Assets/Scripts/Tank/Shoot/Bullet.cs
+++ Assets/Scripts/Tank/Shoot/Bullet.cs
@@ -8,5 +8,23 @@ namespace Tank.Shoot
     {
         public event UnityAction<Bullet> OnBulletDisabled;
 
+        [SerializeField] [Min(0.0f)] private float _speed;
+        
+        private Vector3 _moveDirection;
+
+        private void Start()
+        {
+            _moveDirection = Vector3.forward;
+        }
+
+        private void Update()
+        {
+            MoveBullet();
+        }
+
+        private void MoveBullet()
+        {
+            transform.Translate(_moveDirection * (_speed * Time.deltaTime));
+        }
     }
 }
